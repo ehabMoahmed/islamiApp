23dd360df3edfabedcc38d0b93a0056c7699480c
ehab salah <ehabsalah52t@gmail.com>
2024-02-15 01:17:52 +0200

    +design radio

--- lib/UI/home/radio.dart
+++ lib/UI/home/radio.dart
@@ -6,8 +6,35 @@ class RadioWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 130),
+          child: Image.asset('assets/images/radio.png',width: 412,height: 222),
+        ),
+        Text('إذاعة القرآن الكريم',
+            style: TextStyle(
+                    fontWeight: FontWeight.bold,
+              fontSize: 25,
+        )),
+        SizedBox(height: 50,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+                onTap: (){},
+                child: Image.asset('assets/images/Icon metro-next (1).png')),
+            SizedBox(width: 50),
+            InkWell(
+                onTap: (){},
+                child: Image.asset('assets/images/Icon awesome-play.png')),
+            SizedBox(width: 50),
+            InkWell(
+                onTap: (){},
+                child: Image.asset('assets/images/Icon metro-next.png')),
+
+        ],),
+      ],
     );
   }
 }
